f42404f6aae974c78caa8c03ceca65db7b3b3b7a
Petri Kallio <kallionpetri@gmail.com>
2015-07-06 07:11:47 +0300

    Pieni muutos edelliseen

--- web/app/controllers/omdb_api_controller.js
+++ web/app/controllers/omdb_api_controller.js
@@ -1,15 +1,22 @@
-MovieApp.controller('OmdbApiController', function($scope, OmdbApiService) {
+MovieApp.controller('OmdbApiController', function($scope, OmdbApiService, $interval) {
     $scope.movies = '';
     $scope.searchOn = false;
+    $scope.searchTime = 0;
     
     $scope.findMovie = function() {
         $scope.searchOn = true;
+        
+        var count = $interval(function() {
+            $scope.searchTime++;
+        }, 1000);
         
         OmdbApiService.findMovie($scope.name, $scope.year).success(function(movies) {
             $scope.movies = movies['Search'];
             $scope.name = "";
             $scope.year = "";
             $scope.searchOn = false;
+            $interval.cancel(count);
+            $scope.searchTime = 0;
         });
     };
 });
