31e979fac1b86ebfacb3ce115d02fc336fa9a658
Petri Kallio <kallionpetri@gmail.com>
2015-07-08 00:11:33 +0300

    Bootstrap

--- web/app/services/omdb_api_service.js
+++ web/app/services/omdb_api_service.js
@@ -1,5 +1,5 @@
 MovieApp.service('OmdbApiService', function($http) {
     this.findMovie = function(name, year) {
-        return $http.get('http://www.omdbapi.com', { params: { s: name, y: year } });
+        return $http.get('http://www.omdbapi.com', { params: { s: name, y: year, type: 'movie' } });
     };
 });
